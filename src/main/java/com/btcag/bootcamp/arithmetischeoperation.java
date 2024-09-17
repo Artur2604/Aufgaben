d89194bc932e03a790eaa18e31f31409f6f245ff
ARORT <artur.ort@btc-ag.com>
2024-09-17 15:37:53 +0200

    commit1

---
+++ src/main/java/com/btcag/bootcamp/arithmetischeoperation.java
@@ -0,0 +1,10 @@
+package com.btcag.bootcamp;
+
+public class arithmetischeoperation {
+    public static void main(String[] args) {
+        long zahl=9;
+        System.out.println(zahl++);
+        System.out.println(++zahl);
+
+    }
+}
